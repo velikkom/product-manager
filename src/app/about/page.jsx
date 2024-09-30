99cb07d5c476086141e8766f1d731e539538b04b
Veli Kara <113050906+velikkom@users.noreply.github.com>
2024-09-30 12:48:26 +0300

    routing

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Link from "next/link";
+import path from "path";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>About Page</div>
-  )
-}
+    <div>
+      About Page
+      <p>
+        <Link href="/">Home</Link>
+      </p>
+      <p>
+        <Link href={{
+          pathname: "/products",
+          query: {
+            page: 1,
+            sort: "name",
+            limit: "",
+            order: "asc",
+            status: "active",
+            category: "laptop",
+          },
+        }}
+        
+        >
+          Products
+        </Link>
+      </p>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
